1a87d4a57d074c4e8f5066246b3872c953922bb5
Martz <github@jmartz.de>
2022-02-19 20:44:53 +0100

    fix migration name

--- migrations/2019_10_30_125233_create_form_keys_table.php
+++ migrations/2019_10_30_125233_create_form_keys_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateArticleTable extends Migration
+class CreateFormKeyTable extends Migration
 {
     public string $table = 'form_keys';
     /**
